0bc5588259b34161fa3cf73187152af896af0546
hyeyeon <yeouniverses@gmail.com>
2025-09-21 20:44:45 +0900

    feat: S4. [3-2] Input 공통 컴포넌트 구현하기

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import {StyleSheet, View, Text, SafeAreaView} from 'react-native';
 
+import CustomButton from '@/components/CustomButton';
+import InputField from '@/components/InputField';
+
 function LoginScreen() {
   return (
-    <SafeAreaView>
-      <Text>LoginScreen</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField placeholder="이메일" error="이메일을 입력해주세요." />
+        <InputField placeholder="비밀번호" />
+      </View>
+      <CustomButton label="로그인" variant="filled" size="large" />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
+  },
+});
 
 export default LoginScreen;
