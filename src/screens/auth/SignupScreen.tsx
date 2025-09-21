080de3e79f34d23700d148200bfcaedf170198f1
hyeyeon <yeouniverses@gmail.com>
2025-09-21 20:56:56 +0900

    feat: S4. [3-3] 폼 핸들링하기 (함수화 하기 전)

--- src/screens/auth/SignupScreen.tsx
+++ src/screens/auth/SignupScreen.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, SafeAreaView} from 'react-native';
 
 import CustomButton from '@/components/CustomButton';
 import InputField from '@/components/InputField';
 
 function SignupScreen() {
+  const [email, setEmail] = useState('');
+  const [password, SetPassWord] = useState('');
+  const [passwordConfirm, SetPasswordConfirm] = useState('');
+
+  const handleChangeEmail = (text: string) => {
+    setEmail(text);
+  };
+
+  const handleChangePassword = (text: string) => {
+    SetPassWord(text);
+  };
+
+  const handleChangePasswordConfirm = (text: string) => {
+    SetPasswordConfirm(text);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
-        <InputField placeholder="이메일" />
-        <InputField placeholder="비밀번호" />
+        <InputField
+          placeholder="이메일"
+          value={email}
+          onChangeText={text => handleChangeEmail(text)}
+        />
+        <InputField
+          placeholder="비밀번호"
+          value={password}
+          onChangeText={text => handleChangePassword(text)}
+          secureTextEntry
+        />
+        <InputField
+          placeholder="비밀번호 확인"
+          value={password}
+          onChangeText={text => handleChangePasswordConfirm(text)}
+          secureTextEntry
+        />
         <InputField placeholder="비밀번호 확인" />
       </View>
       <CustomButton label="회원가입" variant="filled" size="large" />
