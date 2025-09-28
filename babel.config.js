f34a4c143b924b69e421af27bff5b3c7aabdac2f
hyeyeon <yeouniverses@gmail.com>
2025-09-28 18:42:34 +0900

    feat: S4. [3-8] 로그인 관련 API 추가하기

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    'react-native-worklets/plugin',
+    'react-native-worklets/plugin', // ← 반드시 첫 번째
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
     [
       'module-resolver',
       {
---
+++ src/types/env.d.ts
@@ -0,0 +1,4 @@
+declare module '@env' {
+  export const ANDROID: string;
+  export const IOS: string;
+}
